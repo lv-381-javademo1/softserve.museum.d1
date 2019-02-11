82c5beb1d496bb41b674ae5931b0d2d90adc9d6a
octap16 <octap17@gmail.com>
2019-02-11 19:40:45 +0200

    render ehibit page in deGet method

---
+++ src/main/java/controller/ExhibitController.java
@@ -0,0 +1,20 @@
+package controller;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ExhibitController extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/views/pages/exhibit.jsp");
+        dispatcher.forward(request, response);
+
+    }
+}
