7e9aa2e772c6eac4acf29e5c16bad28867978de4
octap16 <octap17@gmail.com>
2019-02-11 22:38:36 +0200

    implementation excursionDao findAll()

--- src/main/java/controller/ExcursionController.java
+++ src/main/java/controller/ExcursionController.java
@@ -1,11 +1,16 @@
 package controller;
 
+import dao.ExcursionDao;
+import entity.Excursion;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
 
 
 public class ExcursionController extends HttpServlet {
@@ -14,8 +19,15 @@ public class ExcursionController extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ExcursionDao excursionDao = new ExcursionDao();
+        List<Excursion> excursions;
+        try {
+            excursions = excursionDao.findAll();
+            request.setAttribute("excursions", excursions);
             RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/views/pages/excursion.jsp");
             dispatcher.forward(request, response);
-
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
