a1f2a129512dcc0521d8d7e7ddca1d709fe12f33
Senchakmen <senchakevuch@gmail.com>
2019-02-13 18:35:03 +0200

    Add AuthorController servlet

---
+++ src/main/java/controller/AuthorController.java
@@ -0,0 +1,39 @@
+package controller;
+
+import dao.AuthorDao;
+import entity.Author;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AuthorController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        AuthorDao authorDao = new AuthorDao();
+        List<Author> authorList;
+        try {
+
+            authorList = authorDao.findAll();
+            req.setAttribute("authors", authorList);
+
+            RequestDispatcher dispatcher = req.getRequestDispatcher("WEB-INF/views/pages/author.jsp");
+            dispatcher.forward(req, resp);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
