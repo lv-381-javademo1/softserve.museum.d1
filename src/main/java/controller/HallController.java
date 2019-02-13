a8dc35af96dcc4a89dc4c6e728ddfdf690220877
octap16 <octap17@gmail.com>
2019-02-13 17:38:44 +0200

    render hall view

---
+++ src/main/java/controller/HallController.java
@@ -0,0 +1,34 @@
+package controller;
+
+import dao.HallDao;
+import entity.Hall;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+public class HallController extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HallDao hallDao = new HallDao();
+        List<Hall> halls;
+        try {
+            halls = hallDao.findAll();
+            request.setAttribute("halls", halls);
+
+            RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/views/pages/hall.jsp");
+            dispatcher.forward(request, response);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
