13d304ad3ec0addc1a8d8f900c5e5f4f06ae9bbe
octap16 <octap17@gmail.com>
2019-02-14 15:41:26 +0200

    refactor HallController

--- src/main/java/controller/HallController.java
+++ src/main/java/controller/HallController.java
@@ -13,22 +13,27 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class HallController extends HttpServlet {
+    private HallDao hallDao = new HallDao();
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doGet(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        HallDao hallDao = new HallDao();
-        List<Hall> halls;
+
         try {
+            List<Hall> halls;
             halls = hallDao.findAll();
             request.setAttribute("halls", halls);
-
             RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/views/pages/hall.jsp");
             dispatcher.forward(request, response);
+
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
     }
+
+
 }
