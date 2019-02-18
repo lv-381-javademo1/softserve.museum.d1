f36c42538268bcbde16e8bfe82c8afcdc9c05bba
octap16 <octap17@gmail.com>
2019-02-18 19:11:48 +0200

    delete home page

--- src/main/java/controller/user/HomeController.java
+++ src/main/java/controller/user/HomeController.java
@@ -1,7 +1,4 @@
-package controller;
-
-import dao.ExcursionDao;
-import entity.Excursion;
+package controller.user;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,18 +7,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.sql.SQLException;
-import java.util.List;
 
-@WebServlet("/book_excursion")
-public class BookExcursionController extends HttpServlet {
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        RequestDispatcher dispatcher = req.getRequestDispatcher("WEB-INF/views/pages/bookExcursion.jsp");
+@WebServlet("/")
+public class HomeController extends HttpServlet {
+
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher dispatcher = req.getRequestDispatcher("WEB-INF/views/pages/user/home.jsp");
         dispatcher.forward(req, resp);
-    }
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        int id = Integer.parseInt(request.getParameter("id"));
 
     }
 }
